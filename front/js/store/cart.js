9fa76ede1ae51b2c6160730ffa23c42d27109b38
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2022-06-09 17:49:57 +0200

    adding store JS

--- front/js/store/cart.js
+++ front/js/store/cart.js
@@ -1,1 +1,37 @@
-export const setCart = () => {};
+/**
+ * @typedef {Array} Cart
+ */
+
+const initialCart = [];
+
+/**
+ * @param {Cart} cart
+ */
+export const setCart = async (cart = initialCart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+/**
+ * @returns {Cart}
+ */
+
+export const getCart = async () => {
+  return JSON.parse(localStorage.getItem("cart") || "[]");
+};
+
+/**
+ * @typedef {Object} CartItem
+ * @property {String} product._id
+ * @property {String} product.color
+ * @property {Number} product.quantity
+ * @property {String} product.name
+ */
+
+/**
+ * @param {CartItem} productDto
+ * @returns {CartItem}
+ */
+export const addProductToCart = productDto => {
+  const { productId, color, quantity, name } = productDto;
+  const actualCart = getCart();
+};
--- front/js/utils/fetch-products.js
+++ front/js/utils/fetch-products.js
@@ -1,11 +1,26 @@
 import { API_HOST } from "../config.js";
 
+/**
+ * @typedef {Object} Product
+ * @property {string} _id
+ * @property {string} name
+ * @property {string} description
+ * @property {string} imageUrl
+ * @property {string} altTxt
+ * @property {number} price
+ * @property {string[]} colors
+ */
+
 const fetchParams = {
   method: "GET",
   headers: { "Content-Type": "text/plain;charset=UTF-8" },
   keepalive: false,
 };
 
+/**
+ * @param {string || void} Product._id
+ * @returns {Promise<Product> || Promise<Product[]>}
+ */
 export const getProducts = async productId => {
   const fetchUrl = productId ? `${API_HOST}/${productId}` : API_HOST;
   const data = await fetch(fetchUrl, fetchParams);
