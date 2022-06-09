247175f8c4a34565db8debb8df1a75641f14d034
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2022-06-09 04:45:50 +0200

    http PARAMS for fetch requests

--- front/js/kernel/index.js
+++ front/js/kernel/index.js
@@ -3,22 +3,21 @@ import { getProducts } from "../utils/fetch-products.js";
 const domTarget = "items";
 
 const generateProduct = product => {
-  console.log(product);
   const card = document.createElement("a");
   card.href = `./product.html?id=${product["_id"]}`;
 
   const article = document.createElement("article");
   const image = document.createElement("img");
-  image.src = `image.png`;
-  image.alt = "alternative text";
+  image.src = `${product["imageUrl"]}`;
+  image.alt = product.altTxt;
 
   const title = document.createElement("h3");
   title.setAttribute("class", "productName");
-  title.innerHTML = "product__name";
+  title.innerHTML = product.name;
 
   const desc = document.createElement("p");
   desc.setAttribute("class", "productDescription");
-  desc.innerHTML = "text";
+  desc.innerHTML = product.description;
 
   article.appendChild(image);
   article.appendChild(title);
@@ -36,24 +35,14 @@ const domModifier = () => {
 
 window.onload = async () => {
   const products = await getProducts();
-  const itemsContainer = await domModifier();
+  const itemsContainer = domModifier();
 
-  await products.forEach(async product => {
+  /*await products.forEach(async product => {
     const productArticle = generateProduct(product);
     itemsContainer.appendChild(productArticle);
-  });
+  });*/
 
-  /*products
+  products
     .map(generateProduct)
     .forEach(product => itemsContainer.appendChild(product));
-  domInjector();*/
 };
-
-/*      <a href="./product.html?id=42">
-            <article>
-              <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">Kanap name1</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-            </article>
-          </a> -->
- */
--- front/js/utils/fetch-products.js
+++ front/js/utils/fetch-products.js
@@ -1,13 +1,19 @@
 import { API_HOST } from "../config.js";
 
+const fetchParams = {
+  method: "GET",
+  headers: { "Content-Type": "text/plain;charset=UTF-8" },
+  keepalive: false,
+};
+
 export const getProducts = async () => {
-  const data = await fetch(`${API_HOST}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "text/plain;charset=UTF-8",
-    },
-    keepalive: false,
-  });
+  const data = await fetch(`${API_HOST}`, fetchParams);
+  const res = await data.json();
+  return res;
+};
+
+export const getProductById = async productId => {
+  const data = await fetch(`${API_HOST}/${productId}`, fetchParams);
   const res = await data.json();
   return res;
 };
