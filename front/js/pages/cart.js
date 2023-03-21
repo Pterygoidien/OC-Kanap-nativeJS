3ae26ded9bb0c77d99706829c851a98c12533378
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-03-21 17:14:41 +0100

    changing form in cart.html from POST to get temporarly (HTTP 405 error, don't know why)

--- front/js/pages/cart.js
+++ front/js/pages/cart.js
@@ -11,6 +11,7 @@ window.onload = async () => {
     displayItems(store);
     setTotal(store);
   });
+  formListener();
 };
 
 const displayItems = items => {
@@ -45,6 +46,71 @@ const updateQuantityHandler = event => {
     deleteItemHandler(event);
   }
 };
+
+const formListener = () => {
+  const form = document.querySelector(".cart__order__form");
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const address = document.getElementById("address");
+  const city = document.getElementById("city");
+  const email = document.getElementById("email");
+  const firstNameErrorMsg = document.getElementById("firstNameErrorMsg"); //error message for the first name
+  const lastNameErrorMsg = document.getElementById("lastNameErrorMsg"); //error message for the last name
+  const addressErrorMsg = document.getElementById("addressErrorMsg"); //error message for the address
+  const cityErrorMsg = document.getElementById("cityErrorMsg"); //error message for the city
+  const emailErrorMsg = document.getElementById("emailErrorMsg"); //error message for the email
+
+  //add event listener for the form
+  form.addEventListener("input", (event) => {
+    event.preventDefault();
+    //check if the form is valid
+    if (validateForm()) {
+      //if the form is valid, send the order to the server
+      sendOrder();
+    }
+  });
+
+}
+const validateForm = () => {
+  if (firstName.value.length < 2) {
+    firstNameErrorMsg.innerText = "Le prénom doit contenir au moins 2 caractères";
+    return false;
+  } else {
+    firstNameErrorMsg.innerText = "";
+  }
+  if (lastName.value.length < 2) {
+    lastNameErrorMsg.innerText = "Le nom doit contenir au moins 2 caractères";
+    return false;
+  } else {
+    lastNameErrorMsg.innerText = "";
+  }
+  if (!address.value.match(/^[a-zA-Z0-9\s,.'-]{10,}$/)) {
+    addressErrorMsg.innerText = "L'adresse doit contenir au moins 10 caractères";
+    return false;
+
+  } else {
+    addressErrorMsg.innerText = "";
+  }
+  if (city.value.length < 2) {
+    cityErrorMsg.innerText = "La ville doit contenir au moins 2 caractères";
+    return false;
+  } else {
+    cityErrorMsg.innerText = "";
+  }
+  if (!email.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {  //regex expression from https://www.w3resource.com/javascript/form/email-validation.php !!!
+    emailErrorMsg.innerText = "L'adresse email n'est pas valide";
+    return false;
+  } else {
+    emailErrorMsg.innerText = "";
+  }
+  return true;
+};
+
+
+
+
+
+
 
 const setTotal = cartItems => {
   let totalPrice = 0;
