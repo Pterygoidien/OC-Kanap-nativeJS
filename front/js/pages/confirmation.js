4a9c5dd4cab1a0cad7804013b77ff0d641e0defd
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-03-21 19:50:14 +0100

    trying to select <span id='orderId'> with document.getElementById... and failing at it

--- front/js/pages/confirmation.js
+++ front/js/pages/confirmation.js
@@ -14,11 +14,35 @@ const initContact = () => {
 
 window.onload = async () => {
     const contact = initContact();
-    const product = new Array();
+    const products = new Array();
     const cartItems = getCart();
-    products.forEach(product => {
-        console.log(product._id)
-    })
+    cartItems.forEach(item => {
+        products.push(item._id);
+    });
     const data = { contact, products }
 
+    sendDataToServer(data)
+        .then(order => {
+            if (order.orderId !== undefined) {
+                spanOrderId = document.getElementById("orderId");
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+
+
+}
+
+const sendDataToServer = async (data) => {
+    const response = await fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    const order = await response.json();
+    return order;
 }
