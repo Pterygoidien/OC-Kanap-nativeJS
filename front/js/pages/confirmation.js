a12529ff42bae0e5a976da04fa3fbf79953e0432
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-03-21 19:14:48 +0100

    changing 'confirmation.js' import in 'confirmation.html' as a module to enable import of outside js files' git push

--- front/js/pages/confirmation.js
+++ front/js/pages/confirmation.js
@@ -1,5 +1,16 @@
+import { getLocalStore } from "../store/localStore.js";
+
 console.log('hello');
 
 const urlParams = new URLSearchParams(window.location.search);
-const firstName = urlParams.get("firstName");
-console.log(firstName)
+
+const contact = {
+    firstName: urlParams.get("firstName"),
+    lastName: urlParams.get("lastName"),
+    address: urlParams.get("address"),
+    city: urlParams.get("city"),
+    email: urlParams.get("email")
+}
+const products = getLocalStore();
+console.log(contact)
+
